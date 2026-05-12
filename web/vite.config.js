26772624e90c1df90982b77fda55bb18d0421c90
ApprofittOffro Dev <dev@approfittoffro.local>
2026-05-13 01:09:19 +0200

    Allow production host in Vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "localhost",
+    host: "127.0.0.1",
     port: 5173,
+    allowedHosts: ["lococode.mellutecno.it"],
     proxy: {
       "/api": "http://127.0.0.1:8787"
     }
